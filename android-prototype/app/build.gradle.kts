6ebc8ab7edab4ecb0cc47291b1959f3a8c2cd091
prop4life <prop4life@gmail.com>
2026-09-14 23:43:44 +0530

    Fix Android prototype BuildConfig generation

--- android-prototype/app/build.gradle.kts
+++ android-prototype/app/build.gradle.kts
@@ -14,6 +14,10 @@ android {
         versionName = "0.1.0"
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = true
